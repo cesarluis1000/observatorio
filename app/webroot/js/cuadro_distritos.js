a2b3571788cee50bf470bc0979fad44682da905f
cesarluis1000 <cesarluis1000@hotmail.com>
2019-10-13 22:43:19 -0500

    distrito

--- app/webroot/js/cuadro_distritos.js
+++ app/webroot/js/cuadro_distritos.js
@@ -47,50 +47,15 @@ var config2 = {
 			data: [
 				331,361,314,359,362,273,261,409,177
 			],
-		}, {
-			label: 'VIOLENCIA SEXUAL',
-			fill: false,
-			backgroundColor: window.chartColors.green,
-			borderColor: window.chartColors.green,
-			data: [
-				13,22,34,30,35,32,28,36,14
-			],
-		}, {
-			label: 'ESTAFA',
-			fill: false,
-			backgroundColor: window.chartColors.yellow,
-			borderColor: window.chartColors.yellow,
-			data: [
-				32,33,34,37,43,25,32,36,10
-			],
-		}, {
-			label: 'DROGAS',
-			fill: false,
-			backgroundColor: window.chartColors.purple,
-			borderColor: window.chartColors.purple,
-			data: [
-				13,6,17,12,15,14,11,15,8
-			],
-		}, {
-			label: 'SECUESTRO',
-			fill: false,
-			backgroundColor: window.chartColors.yellow,
-			borderColor: window.chartColors.yellow,
-			data: [
-				0,2,0,1,1,3,3,4,0
-			],
-		}, {
-			label: 'HOMICIDIO',
-			fill: false,
-			backgroundColor: window.chartColors.gray,
-			borderColor: window.chartColors.gray,
-			data: [
-				4,7,3,7,7,4,0,0,0
-			],
 		}]
 	},
 	options: {
 		responsive: true,
+		elements: {
+			line: {
+				tension: 0.000001
+			}
+		},
 		title: {
 					display: true,
 					text: 'Distrito Cercado Lima'
